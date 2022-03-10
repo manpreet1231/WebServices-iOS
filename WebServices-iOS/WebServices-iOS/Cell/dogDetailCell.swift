5538c9a1d42beb038059d9c5209d63dac9f7806c
Tajamal Iqbal <96197943+manpreet1231@users.noreply.github.com>
2022-03-10 22:59:03 +0500

    Design+Outlets

---
+++ WebServices-iOS/WebServices-iOS/Cell/dogDetailCell.swift
@@ -0,0 +1,8 @@
+//
+//  dogDetailCell.swift
+//  WebServices-iOS
+//
+//  Created by Tajamal on 10/03/2022.
+//
+
+import Foundation
--- WebServices-iOS/WebServices-iOS/Controllers/ViewController.swift
+++ WebServices-iOS/WebServices-iOS/Controllers/ViewController.swift
@@ -8,12 +8,15 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var tblDogsInfo: UITableView!
+    @IBOutlet weak var containerView: UIView!
+    
+     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+       
     }
-
-
+    
 }
 
