dfbc9d4f57d01cd2ddfede62ad3d1e5e935df35a
Tajamal Iqbal <96197943+manpreet1231@users.noreply.github.com>
2022-03-10 23:19:32 +0500

    DogsName & Images in TableView Cell

--- WebServices-iOS/WebServices-iOS/Cell/dogDetailCell.swift
+++ WebServices-iOS/WebServices-iOS/Cell/dogDetailCell.swift
@@ -6,3 +6,46 @@
 //
 
 import Foundation
+import UIKit
+class dogsInfoCell: UITableViewCell {
+
+    @IBOutlet weak var imgDog: UIImageView!
+    @IBOutlet weak var lblDogBreadName: UILabel!
+    
+   
+    @IBOutlet weak var containerView: UIView!
+    
+    //Set The Dog Name
+    var dogNameSet: String? = ""{
+        didSet{
+            lblDogBreadName.text = dogNameSet ?? ""
+        }
+    }
+    
+    //Srty The Dog Image
+    var dogImageSet: UIImage? = nil{
+        didSet{
+            if let img = dogImageSet{
+                imgDog.image = img
+            }
+        }
+    }
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+        imgDog.layer.cornerRadius = 8
+        lblDogBreadName.numberOfLines = 2
+        containerView.layer.cornerRadius = 8
+        containerView.layer.borderWidth = 1
+        containerView.layer.borderColor = #colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1)
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- WebServices-iOS/WebServices-iOS/Controllers/ViewController.swift
+++ WebServices-iOS/WebServices-iOS/Controllers/ViewController.swift
@@ -12,11 +12,91 @@ class ViewController: UIViewController {
     @IBOutlet weak var tblDogsInfo: UITableView!
     @IBOutlet weak var containerView: UIView!
     
-     
+    
+    var arraybreeds = [String]()
+    var images = [UIImage]()
+    var valueToPass:String!
+    var valueToPassImage:UIImage!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-       
+        fetchBreadName()
     }
     
+    // This function is called before the segue
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get a reference to the second view controller
+        let secondViewController = segue.destination as! previewImageController
+        // Set a variable in the second view controller with the" String to pass
+        
+    }
 }
+
+//Table view Configure data
+extension  ViewController :UITableViewDataSource,UITableViewDelegate{
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return arraybreeds.count //total breeds count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell") as! dogsInfoCell
+        
+        cell.dogNameSet = "\(indexPath.row + 1) - \(arraybreeds[indexPath.row].uppercased())"
+        
+        if images.count != 0{
+            cell.dogImageSet = images[indexPath.row]
+        }
+        return cell
+        
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 100 //height of cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        valueToPass = "\(arraybreeds[indexPath.row])"
+        valueToPassImage = (images[indexPath.row])
+        performSegue(withIdentifier: "dogsDetail", sender: self)
+    }
+}
+
+//Fectch Bread And images
+extension ViewController{
+    
+    //Fecth information from api
+    func fetchBreadName(){
+        
+        DogAPIHelpers.fetchBreeds { [self]
+            breeds in
+            for (key, _) in breeds {
+                self.arraybreeds.append(key)
+            }
+            tblDogsInfo.reloadData()
+            fetchBreadImage()
+        }
+    }
+    
+    //Fecth Random Image information of Bread
+    func fetchBreadImage() {
+        arraybreeds.forEach { (dogsBread) in
+            ActivityController.shared.showActivityIndicator(uiView: containerView)
+            DogAPIHelpers.fetchBreedImage(breed: dogsBread){ [self]
+                Images in
+                images.append(Images)
+                relodData()
+            }
+        }
+    }
+    
+    //Reload TableView After Fetch The Image of Bread
+    func  relodData() {
+        if images.count == arraybreeds.count{
+            tblDogsInfo.reloadData()
+            ActivityController.shared.hideActivityIndicator(uiView: containerView)
+        }
+    }
+}
+
 
